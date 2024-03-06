f1888e3d177c67d56245dcba778018df896671f0
ShayanBiswas <info.sbriju@gmail.com>
2024-03-06 19:57:22 +0530

    hi

---
+++ src/components/EarlyAccess.js
@@ -0,0 +1,159 @@
+
+import React, { useState, useEffect } from "react";
+import { FaEdit, FaTrash, FaTimes } from "react-icons/fa";
+
+function EarlyAccess() {
+    const [showModal, setShowModal] = useState(false);
+    const [formData, setFormData] = useState({
+        id: "",
+        employeeName: "",
+        email: "",
+        mobileNumber: "",
+        dob: "",
+        gender: "",
+        country: "",
+    });
+    const [employees, setEmployees] = useState([]);
+    const [selectedEmployeeId, setSelectedEmployeeId] = useState(null);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(
+                "https://crudcrud.com/api/3b6744664e6148eb8655f85ad9942e0f/posts"
+            );
+            const data = await response.json();
+            setEmployees(data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            let url =
+                "https://crudcrud.com/api/3b6744664e6148eb8655f85ad9942e0f/posts";
+            let method = "POST";
+            if (selectedEmployeeId) {
+                url += `/${selectedEmployeeId}`;
+                method = "PUT";
+            }
+            const response = await fetch(url, {
+                method: method,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await response.json();
+            console.log("Success:", data);
+            setFormData({
+                id: "",
+                employeeName: "",
+                email: "",
+                mobileNumber: "",
+                dob: "",
+                gender: "",
+                country: "",
+            });
+            setShowModal(false);
+            setSelectedEmployeeId(null);
+            fetchData();
+        } catch (error) {
+            console.error("Error:", error);
+        } finally {
+            setShowModal(false);
+        }
+    };
+
+    return (
+        <div className="app">
+            <button className="add-employee-btn" onClick={() => setShowModal(true)}>
+                Early Access
+            </button>
+
+            {showModal && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <form onSubmit={handleSubmit}>
+                            <span className="close-btn" onClick={() => setShowModal(false)}>
+                                <FaTimes />
+                            </span>
+                            <p>Early Access</p>
+                            <br />
+                            <label> Full Name</label>
+                            <input
+                                type="text"
+                                name="employeeName"
+                                value={formData.employeeName}
+                                onChange={handleChange}
+                                placeholder="Enter Employee Name"
+                                required
+                            />
+
+                            <label>Email Address</label>
+                            <input
+                                type="email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                placeholder="Enter Email Address"
+                                required
+                            />
+
+                            <label>Mobile Number</label>
+                            <input
+                                type="tel"
+                                name="mobileNumber"
+                                value={formData.mobileNumber}
+                                onChange={handleChange}
+                                placeholder="Enter Mobile Number"
+                                required
+                            />
+                            <label>Gender:</label>
+                            <select
+                                name="gender"
+                                value={formData.gender}
+                                onChange={handleChange}
+                                required
+                            >
+                                <option value="">Select Gender</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                                <option value="other">Other</option>
+                            </select>
+
+                            <label>Country</label>
+                            <select
+                                name="country"
+                                value={formData.country}
+                                onChange={handleChange}
+                                required
+                            >
+                                <option value="">Select Country</option>
+                                <option value="Afghanistan">Afghanistan</option>
+                                <option value="Albania">Albania</option>
+                                <option value="Albania">India</option>
+                            </select>
+
+                            <button type="submit">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default EarlyAccess;
---
+++ src/components/HomePage.js
@@ -0,0 +1,115 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from "react";
+import EarlyAccess from "./EarlyAccess";
+
+function HomePage() {
+  return (
+    <div>
+      <header>
+        <span className="logo">Logo</span>
+        <span className="header_right_btn"><EarlyAccess /></span>
+      </header>
+      <main>
+        <div className="main_container">
+          <div className="sub_container_one">
+            <div className="sub_container_one_left">
+              <div className="heading_container">
+                <p className="heading">meetX is </p>
+                <p className="heading">now on the </p>
+                <p className="heading"> web!</p>
+              </div>
+              <div className="desc">
+                <p className="desc_1">Chat, Snap, and video call your</p>
+                <p className="desc_1">friends from wherever you are.</p>
+              </div>
+              <div className="desc">
+                <span className="desc_2_pre"> NEW </span>
+                <span className="desc_2_post">
+                  Message My AI, your personal
+                </span>
+                <p className="desc_2_post">chatbot sidekick!</p>
+              </div>
+            </div>
+            <div className="sub_container_one_right">
+              <img src="./images/meetx.png" alt="meetx" />
+            </div>
+          </div>
+          <div className="sub_container_one">
+            <div className="sub_container_one_right">
+              <img src="./images/meetx.png" alt="meetx" />
+            </div>
+            <div className="sub_container_one_left">
+              <div className="heading_container">
+                <p className="heading">meetX is </p>
+                <p className="heading">now on the </p>
+                <p className="heading"> web!</p>
+              </div>
+              <div className="desc">
+                <p className="desc_1">Chat, Snap, and video call your</p>
+                <p className="desc_1">friends from wherever you are.</p>
+              </div>
+              <div className="desc">
+                <span className="desc_2_pre"> NEW </span>
+                <span className="desc_2_post">
+                  Message My AI, your personal
+                </span>
+                <p className="desc_2_post">chatbot sidekick!</p>
+              </div>
+            </div>
+          </div>
+          <div className="sub_container_one">
+            <div className="sub_container_one_left">
+              <div className="heading_container">
+                <p className="heading">meetX is </p>
+                <p className="heading">now on the </p>
+                <p className="heading"> web!</p>
+              </div>
+              <div className="desc">
+                <p className="desc_1">Chat, Snap, and video call your</p>
+                <p className="desc_1">friends from wherever you are.</p>
+              </div>
+              <div className="desc">
+                <span className="desc_2_pre"> NEW </span>
+                <span className="desc_2_post">
+                  Message My AI, your personal
+                </span>
+                <p className="desc_2_post">chatbot sidekick!</p>
+              </div>
+            </div>
+            <div className="sub_container_one_right">
+              <img src="./images/meetx.png" alt="meetx" />
+            </div>
+          </div>
+          <div className="sub_container_one">
+            <div className="sub_container_one_right">
+              <img src="./images/meetx.png" alt="meetx" />
+            </div>
+            <div className="sub_container_one_left">
+              <div className="heading_container">
+                <p className="heading">meetX is </p>
+                <p className="heading">now on the </p>
+                <p className="heading"> web!</p>
+              </div>
+              <div className="desc">
+                <p className="desc_1">Chat, Snap, and video call your</p>
+                <p className="desc_1">friends from wherever you are.</p>
+              </div>
+              <div className="desc">
+                <span className="desc_2_pre"> NEW </span>
+                <span className="desc_2_post">
+                  Message My AI, your personal
+                </span>
+                <p className="desc_2_post">chatbot sidekick!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <p>© 2024 meetX. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default HomePage;
